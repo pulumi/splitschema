24b7f2d85b0a812c8adaa1ace828348c6efd3a32
Daniel Bradley <daniel@pulumi.com>
2024-04-05 13:41:38 +0100

    Add Azure test data

--- e2e_testdata.go
+++ e2e_testdata.go
@@ -13,39 +13,45 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/pulumi/splitschema"
 )
 
-var awsVersion = "v6.29.0"
-
 func main() {
 	if err := os.MkdirAll("testdata", 0755); err != nil {
 		panic(err)
 	}
-	bytes, err := downloadAwsSchema()
-	if err != nil {
+	if err := prepareProviderSchema("aws", "v6.29.0"); err != nil {
 		panic(err)
+	}
+	if err := prepareProviderSchema("azure-native", "v2.35.0"); err != nil {
+		panic(err)
+	}
+}
+
+func prepareProviderSchema(provider, version string) error {
+	bytes, err := downloadSchema(provider, version)
+	if err != nil {
+		return err
 	}
 
 	var pkg schema.PackageSpec
 	err = json.Unmarshal(bytes, &pkg)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	if err = os.WriteFile("testdata/aws.json", bytes, 0644); err != nil {
-		panic(err)
+	if err = os.WriteFile(filepath.Join("testdata", provider+".json"), bytes, 0644); err != nil {
+		return err
 	}
 
-	if err := splitschema.WritePackageSpec("testdata/aws", &pkg); err != nil {
-		panic(err)
-	}
+	return splitschema.WritePackageSpec(filepath.Join("testdata", provider), &pkg)
 }
 
-func downloadAwsSchema() ([]byte, error) {
-	res, err := http.Get(fmt.Sprintf("https://raw.githubusercontent.com/pulumi/pulumi-aws/%s/provider/cmd/pulumi-resource-aws/schema.json", awsVersion))
+func downloadSchema(provider, version string) ([]byte, error) {
+	res, err := http.Get(fmt.Sprintf("https://raw.githubusercontent.com/pulumi/pulumi-%s/%s/provider/cmd/pulumi-resource-%s/schema.json", provider, version, provider))
 	if err != nil {
 		return nil, err
 	}
